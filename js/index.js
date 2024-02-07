ac98c3359ee50a742d400673b8f3784e33b5b270
ahdbot <ahdbot3@gmail.com>
2024-02-07 11:25:00 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,64 @@
+
+
+
+function scrollFnuction()
+{
+    if (document.documentElement.scrollTop > 200) {
+        document.getElementById('navbar').classList.add('noTransparrent');
+    } else {
+        document.getElementById('navbar').classList.remove('noTransparrent');
+    }
+};
+
+
+window.onscroll = function() {
+    scrollFnuction();
+
+}
+
+window.onload = function () {
+    let website = new CountUp('website-count', 0, 250, 0, 2.5);
+    website.start();
+    let app = new CountUp('app-count', 0, 100, 0, 2.5);
+    app.start();
+    let user = new CountUp('user-count', 0, 1000, 0, 2.5);
+    user.start();
+    let video = new CountUp('video-count', 0, 30, 0, 2.5);
+    video.start();
+  };
+
+  function validate() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+}
+validate();
+
+
+document.getElementById("fullyear").innerHTML = new Date().getFullYear();
+
+
+
+$(document).ready(function () {
+
+  $('.close').click(function() {
+  
+    $(".gallery-overlay").toggle();
+});
+
+});
